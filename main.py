cef6b9d58716821d4840ed8269cd75522d8c64cd
JP Lacy <lacyjpr@gmail.com>
2015-09-08 14:01:56 -0700

    Add Guestbook example code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import os
 import jinja2
+import os
+import urllib
 import webapp2
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
-
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -17,10 +19,6 @@ class Handler(webapp2.RequestHandler):
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
-
-class MainPage(Handler):
-    def get(self):
-        self.render("IPND_notes.html")
 
 class StageOne(Handler):
     def get(self):
@@ -38,10 +36,81 @@ class StageFour(Handler):
     def get(self):
         self.render("stage_four.html")
 
+DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+
+def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+    """Constructs a Datastore key for a Guestbook entity.
+
+    We use guestbook_name as the key.
+    """
+    return ndb.Key('Guestbook', guestbook_name)
+
+class Author(ndb.Model):
+    """Sub model for representing an author."""
+    identity = ndb.StringProperty(indexed=False)
+    email = ndb.StringProperty(indexed=False)
+
+class Greeting(ndb.Model):
+    """A main model for representing an individual Guestbook entry."""
+    author = ndb.StructuredProperty(Author)
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+class MainPage(Handler):
+    def get(self):
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
+        greetings_query = Greeting.query(
+            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(10)
+
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'user': user,
+            'greetings': greetings,
+            'guestbook_name': urllib.quote_plus(guestbook_name),
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = jinja_env.get_template('IPND_notes.html')
+        self.response.write(template.render(template_values))
+#        self.render("IPND_notes.html")
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        # We set the same parent key on the 'Greeting' to ensure each
+        # Greeting is in the same entity group. Queries across the
+        # single entity group will be consistent. However, the write
+        # rate to a single entity group should be limited to
+        # ~1/second.
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
+
+        if users.get_current_user():
+            greeting.author = Author(
+                    identity=users.get_current_user().user_id(),
+                    email=users.get_current_user().email())
+
+        greeting.content = self.request.get('content')
+        greeting.put()
+
+        query_params = {'guestbook_name': guestbook_name}
+        self.redirect('/?' + urllib.urlencode(query_params))
+
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/stage1', StageOne),
                                ('/stage2', StageTwo),
                                ('/stage3', StageThree),
                                ('/stage4', StageFour),
+                               ('/sign', Guestbook)
                                ],
                               debug=True)
