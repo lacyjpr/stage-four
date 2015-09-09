60714b664c0ca071020214c69b3d0c9fb3e7865b
JP Lacy <lacyjpr@gmail.com>
2015-09-09 05:30:39 -0700

    Add comments to main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
 
+# Copied from Udacity lectures
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -45,25 +46,34 @@ def bulletinboard_key(bulletinboard_name=DEFAULT_BULLETINBOARD_NAME):
     """
     return ndb.Key('Bulletinboard', bulletinboard_name)
 
+# Copied from Udacity's wallbook example. Eliminated name for simplicity.
 class Author(ndb.Model):
     """Sub model for representing an author."""
     identity = ndb.StringProperty(indexed=False)
     email = ndb.StringProperty(indexed=False)
 
+# Copied from Udacity's wallbook example
 class Greeting(ndb.Model):
     """A main model for representing an individual Bulletinboard entry."""
     author = ndb.StructuredProperty(Author)
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
+# Largely borrowed from Google's example code
 class MainPage(Handler):
+    """Builds the main page including the bulletin board"""
     def get(self):
+# Mark's error variable from the Udacity webcast
         error = self.request.get('error','')
         bulletinboard_name = self.request.get('bulletinboard_name',
                                           DEFAULT_BULLETINBOARD_NAME)
+
+# Query Datastore for comments ordered by date ascending
+# store the results in comments_query
         greetings_query = Greeting.query(
             ancestor=bulletinboard_key(bulletinboard_name)).order(-Greeting.date)
 
+#Fetch comments from our query, store them in the variable comments
         greetings_to_fetch = 20
         greetings = greetings_query.fetch(greetings_to_fetch)
 
@@ -74,6 +84,8 @@ class MainPage(Handler):
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
+
+# Parameters to pass to the template
         template_values = {
             'user': user,
             'greetings': greetings,
@@ -83,10 +95,12 @@ class MainPage(Handler):
             'error': error,
         }
 
+# Render our page
         template = jinja_env.get_template('IPND_notes.html')
         self.write(template.render(template_values))
 
-class Guestbook(Handler):
+#largely borrowed from Google's example code
+class BulletinBoard(Handler):
     def post(self):
         # We set the same parent key on the 'Greeting' to ensure each
         # Greeting is in the same entity group. Queries across the
@@ -97,16 +111,19 @@ class Guestbook(Handler):
                                           DEFAULT_BULLETINBOARD_NAME)
         greeting = Greeting(parent=bulletinboard_key(bulletinboard_name))
 
+# If the user is logged in instantiate the Author class
         if users.get_current_user():
             greeting.author = Author(
                     identity=users.get_current_user().user_id(),
                     email=users.get_current_user().email())
 
         greeting.content = self.request.get('content')
+# Validate content exists and is not blank, if so put to Datastore
         if greeting.content and greeting.content.isspace() == False:
             greeting.put()
             self.redirect('/')
         else:
+# Mark's error message from the Udacity webcase
             self.redirect('/?error=Error, please input text!')
 
 
@@ -115,6 +132,6 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/stage2', StageTwo),
                                ('/stage3', StageThree),
                                ('/stage4', StageFour),
-                               ('/sign', Guestbook)
+                               ('/sign', BulletinBoard)
                                ],
                               debug=True)
